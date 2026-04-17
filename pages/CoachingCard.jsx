a852b5770c27a58a82cef7086ec2dac088c2d211
connect668 <connect@initiativeenterprises.net>
2026-04-17 16:08:20 -0500

    Fix import path for dashboardStyles in CoachingCard

--- pages/CoachingCard.jsx
+++ pages/CoachingCard.jsx
@@ -1,4 +1,4 @@
-import { styles } from "../../utils/dashboardStyles";
+import { styles } from "../utils/dashboardStyles";
 
 export default function CoachingCard({ item, formatDateFn }) {
   return (
